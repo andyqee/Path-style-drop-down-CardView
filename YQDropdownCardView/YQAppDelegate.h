b8d9cd8c8869596b85a122896c80678a55b5e4ca
andyqee <vitasone@gmail.com>
2014-08-06 20:47:24 +0800

    Initial Commit

---
+++ YQDropdownCardView/YQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YQAppDelegate.h
+//  YQDropdownCardView
+//
+//  Created by andy on 6/8/14.
+//  Copyright (c) 2014 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YQDropdownCardView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YQDropdownCardView
+//
+//  Created by andy on 6/8/14.
+//  Copyright (c) 2014 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YQAppDelegate class]));
+    }
+}
